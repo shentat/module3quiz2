c303a240e316b3717c37ff8d4ac3376e13e8a5f7
shentat <stgoh@i2r.a-star.edu.sg>
2015-03-12 19:32:38 +0800

    Update getDate.r

--- getDate.r
+++ getDate.r
@@ -8,14 +8,15 @@ oauth_endpoints("github")
 #
 # Insert your client ID and secret below - if secret is omitted, it will
 # look it up in the GITHUB_CONSUMER_SECRET environmental variable.
-myapp <- oauth_app("github", "2f48e44d8b234bb49b8d")
+myapp <- oauth_app("github", "2f48e44d8b234bb49b8d", "762e3f573ae6db005d1b20d8a8f041200733e12f")
 # 3. Get OAuth credentials
 github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
 # 4. Use API
 gtoken <- config(token = github_token)
-req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+req <- GET("https://api.github.com/rate_limit", gtoken)
 stop_for_status(req)
 content(req)
+BROWSE("https://api.github.com/users/jtleek/repos",authenticate("Access Token","x-oauth-basic","basic"))
 # OR:
 #req <- with_config(gtoken, GET("https://api.github.com/users/jtleek/repos"))
 #stop_for_status(req)
